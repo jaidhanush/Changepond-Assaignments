5bed003f4c929e6e3d5adb1a117753cff79eda0d
jayakumar <jaidhanushdj@gmail.com>
2024-12-27 18:25:14 +0530

    day4

--- Python/MIni_Task/Task_D4.py
+++ Python/MIni_Task/Task_D4.py
@@ -1,15 +1,25 @@
 
 
-my_list=[1,2,3,4,2,3]
+my_list=[1,2,3,4,2,3,3]
+
+temp=0
+for i in range(len(my_list)):
+        if(my_list[i]==3):
+            temp=temp+1
 
 
-def check(val):
-    for i in my_list:
-        if(i==val):
-            return val
+print(temp)
+
+# def check(val):
+    # for i in range(len(my_list)):
+    #     temp=0
+    #     if(my_list[i]==val):
+    #         temp=temp+1
+            
+    # return temp
         
     
 # for i in my_list:
 #     check(i)
         
-print(check(3))
+# print(check(2))
