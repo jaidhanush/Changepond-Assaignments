f29e2dd92f42ce1e545bcced063d2753b1ada6be
jai <jaidhanushdj@gmail.com>
2025-01-06 22:45:35 +0530

    form update

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Post
 from django.shortcuts import get_object_or_404
+from .forms import BloggerForm, PostForm
+
+
+def create_post(request):
+    if request.method == 'POST':
+        blogger_form = BloggerForm(request.POST)
+        post_form = PostForm(request.POST, request.FILES)
+        if blogger_form.is_valid() and post_form.is_valid():
+            blogger = blogger_form.save()
+            post = post_form.save(commit=False)
+            post.blogger = blogger
+            post.save()
+            return redirect('homepage')
+    else:
+        blogger_form = BloggerForm()
+        post_form = PostForm()
+    
+    return render(request, 'blog/create_post.html', {
+        'blogger_form': blogger_form,
+        'post_form': post_form
+    })
+
 
 def homepage(request):
     latest_posts = Post.objects.all().order_by('-date')[:3]
@@ -14,3 +36,7 @@ def all_blogs(request):
 def blog_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
     return render(request, 'blog/blog_detail.html', {'post': post})
+
+from django.shortcuts import render, redirect
+from .forms import BloggerForm, PostForm
+
